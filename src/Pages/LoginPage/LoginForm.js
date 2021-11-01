775176900b6004668dbfe91ec0da5facaa6255ef
Érico Marshall <ericogmarshall1@gmail.com>
2021-11-01 18:28:56 -0300

    loginPage, signUpPage, hooks de protected e unprotected e services de user feitos

---
+++ src/Pages/LoginPage/LoginForm.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import useForm from '../../Hooks/useForm';
+import {  LoginButton, InputsContainer } from './styled';
+import { TextField } from '@material-ui/core';
+import { login } from "../../Services/user";
+import { useHistory } from 'react-router-dom';
+
+const LoginForm = () => {
+    const [form, onChange, clearFields] = useForm({email: "", password: ""});
+    const history = useHistory();
+
+    const onSubmitForm = (event) => {
+        event.preventDefault();
+        login(form, clearFields, history);
+    };
+
+    return (
+        <InputsContainer>
+            <form onSubmit={onSubmitForm}>
+                <TextField
+                    name={"email"}
+                    value={form.email}
+                    onChange={onChange}
+                    label={"E-mail"} 
+                    variant={"outlined"}
+                    fullWidth
+                    margin={"normal"}
+                    type={"email"}
+                    placeholder={"email@email.com"}
+                />
+                <TextField
+                    name={"password"}
+                    value={form.password}
+                    onChange={onChange}
+                    label={"Senha"}
+                    variant={"outlined"} 
+                    fullWidth
+                    margin={"normal"}
+                    type={"password"}
+                    placeholder={"Mínimo de 6 caracteres"}
+                />
+                <LoginButton>
+                    Entrar
+                </LoginButton>
+            </form>
+        </InputsContainer>
+    )
+}
+
+export default LoginForm;
---
+++ src/Services/user.js
@@ -0,0 +1,30 @@
+import axios from "axios";
+import { goToHome } from "../Routes/coordinator";
+
+export const login = (body, clearFields, history) => {
+    axios
+    .post(`https://us-central1-missao-newton.cloudfunctions.net/rappi4A/login`, body)
+    .then((response) => {
+        localStorage.setItem("token", response.data.token);
+        clearFields();
+        goToHome(history);
+    })
+    .catch((error) => {
+        alert("Erro ao fazer o login, tente novamente!");
+        console.log(error.response.data.message);
+    })
+};
+
+export const signUp = (body, clearFields, history) => {
+    axios
+    .post(`https://us-central1-missao-newton.cloudfunctions.net/rappi4A/signup`, body)
+    .then((response) => {
+        localStorage.setItem("token", response.data.token);
+        clearFields();
+        goToHome(history);
+    })
+    .catch((error) => {
+        alert("Erro ao fazer o login, tente novamente!");
+        console.log(error.response.data.message);
+    })
+};
