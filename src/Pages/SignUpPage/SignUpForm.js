6e15d2c973092d08445fee1a36fbe35c4233b126
Érico Marshall <ericogmarshall1@gmail.com>
2021-11-04 16:32:40 -0300

    mascara do cpf funcionando

--- src/Pages/SignUpPage/SignUpForm.js
+++ src/Pages/SignUpPage/SignUpForm.js
@@ -43,7 +43,8 @@ const SignUpForm = () => {
     };
 
     const maskedCPF = (cpf) => {
-        let maskedValue = cpf.replace(/\D+/g,"");
+        // let maskedValue = cpf.replace(/\D+/g,"");
+        let maskedValue = cpf.replace(/\D/g, '').replace(/(\d{3})(\d)/, '$1.$2').replace(/(\d{3})(\d)/, '$1.$2').replace(/(\d{3})(\d{1,2})/, '$1-$2').replace(/(-\d{2})\d+?$/, '$1')
         return maskedValue;
     }
 
