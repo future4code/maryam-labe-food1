e24a0b3ad3d94b3d7120592ad3b91560bc95c538
Érico Marshall <ericogmarshall1@gmail.com>
2021-11-04 16:33:41 -0300

    limpeza do código de signUpForm

--- src/Pages/SignUpPage/SignUpForm.js
+++ src/Pages/SignUpPage/SignUpForm.js
@@ -43,10 +43,9 @@ const SignUpForm = () => {
     };
 
     const maskedCPF = (cpf) => {
-        // let maskedValue = cpf.replace(/\D+/g,"");
         let maskedValue = cpf.replace(/\D/g, '').replace(/(\d{3})(\d)/, '$1.$2').replace(/(\d{3})(\d)/, '$1.$2').replace(/(\d{3})(\d{1,2})/, '$1-$2').replace(/(-\d{2})\d+?$/, '$1')
         return maskedValue;
-    }
+    };
 
     return (
         <InputsContainer>
