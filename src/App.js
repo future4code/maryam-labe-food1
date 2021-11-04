dec0256a443c6be65bce316970a727d0654b25b0
Glauber Apolinario de Freitas <glauber.apolinario@yahoo.com.br>
2021-11-04 11:16:10 -0300

    Adição do context para uso de variaveis globais

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Router from './Routes/Routers';
+import MyContext from './Contexts/myContext'
 
 const App = () => {
+  const [cart, setCart] = useState('')
 
   return (
+    <MyContext.Provider value ={{cart, setCart}}>
       <Router />
+    </MyContext.Provider>
   );
 }
 
